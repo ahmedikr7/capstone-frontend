1a82bc15aabc03f895794465320972781b9bd601
Ahmed Ikram <51349520+ahmedikr7@users.noreply.github.com>
2022-09-25 23:50:29 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Header from "../src/header/Header.jsx";
+import Home from "../src/pages/homepage/Home.jsx";
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
